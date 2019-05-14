a2160091628bdf4cb9d01153c2d4358bab3a18d3
shaneg1507 <39335552+shaneg1507@users.noreply.github.com>
2019-05-14 12:34:10 +0100

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,60 @@
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+from nltk import wordpunct_tokenize
+from nltk.corpus import stopwords
+
+def _calculate_languages_ratios(text):
+    """
+    Calculate probability of given text to be written in several languages and
+    return a dictionary that looks like {'french': 2, 'spanish': 4, 'english': 0}
+    
+    @param text: Text whose language want to be detected
+    @type text: str
+    
+    @return: Dictionary with languages and unique stopwords seen in analyzed text
+    @rtype: dict
+    """
+
+    languages_ratios = {}
+
+    #nltk.wordpunct_tokenize() splits all punctuations into separate tokens
+    tokens = wordpunct_tokenize(text)
+    words = [word.lower() for word in tokens]
+
+    # Compute per language included in nltk number of unique stopwords appearing in analyzed text
+    for language in stopwords.fileids():
+        stopwords_set = set(stopwords.words(language))
+        words_set = set(words)
+        common_elements = words_set.intersection(stopwords_set)
+
+        languages_ratios[language] = len(common_elements) # language "score"
+
+    return languages_ratios
+
+
+#----------------------------------------------------------------------
+def detect_language(text):
+    """
+    Calculate probability of given text to be written in several languages and
+    return the highest scored.
+    
+    It uses a stopwords based approach, counting how many unique stopwords
+    are seen in analyzed text.
+    
+    @param text: Text whose language want to be detected
+    @type text: str
+    
+    @return: Most scored language guessed
+    @rtype: str
+    """
+
+    ratios = _calculate_languages_ratios(text)
+
+    most_rated_language = max(ratios, key=ratios.get)
+
+    return most_rated_language
+
+
+
